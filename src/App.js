9b489ac50ecf4271aa39a7e86c6fc3def3626179
pchanrobles <carloescot@gmail.com>
2021-11-23 20:54:31 +0100

    sass

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+
+
+
+export const App = () => {
+  return (
+    <div className='container'>
+    <h1 className='title'>Uso de SASS</h1>
+  <hr />
+  <ul>
+    <li>
+      <a href='#'>Github</a>  
+      
+    </li>
+    <li>
+    <a href='#'>Linkedin</a>
+    </li>
+    <li>
+    <a href='#'>Google</a>
+    </li>
+  </ul>
+
+<hr />
+<div className='container'>
+  <h2>Contador: 0</h2>   
+
+<button className='btn btn-primary'>+1</button>
+</div>
+
+  
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = ({text}) => {
+    return (
+        <>
+            <Button className='type'>{text} </Button>
+        </>
+    )
+}
+
+export default Button
